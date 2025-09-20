8f950be32da4186c4ee685c8f5125f0ed107ccac
ru-iqsay <109206080+ru-iqsay@users.noreply.github.com>
2025-09-20 13:38:41 +0530

    working

--- backend/config.py
+++ backend/config.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/star_tailors'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://startailor657_db_user:z9fWtfJ4auOj4KpT@cluster0.sfkrwck.mongodb.net/'
     JWT_ACCESS_TOKEN_EXPIRES = 86400  # 24 hours in seconds
-
+# z9fWtfJ4auOj4KpT  mongodb+srv://startailor657_db_user:z9fWtfJ4auOj4KpT@cluster0.sfkrwck.mongodb.net/
 class DevelopmentConfig(Config):
     DEBUG = True
 
