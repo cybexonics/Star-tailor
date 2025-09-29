f20f31240109b90d04f14125dfabc44468b41ede
cybexonics <cybexonicsitconsultants@gmail.com>
2025-09-29 22:35:07 +0530

    Create index.py

---
+++ api/customers/index.py
@@ -0,0 +1,28 @@
+import json
+
+def handler(request):
+    # Handle CORS preflight
+    if request["method"] == "OPTIONS":
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+            "body": ""
+        }
+
+    # Return mock list of customers
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+        },
+        "body": json.dumps({
+            "customers": []
+        })
+    }
