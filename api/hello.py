19240daf6184f372d7a5cc4eac537e886e347857
cybexonics <cybexonicsitconsultants@gmail.com>
2025-09-29 19:48:26 +0530

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,9 +1,12 @@
-from http.server import BaseHTTPRequestHandler
 import json
 
 def handler(request):
     return {
         "statusCode": 200,
-        "headers": { "Content-Type": "application/json" },
-        "body": json.dumps({ "message": "Hello from Python on Vercel!" })
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Hello from Python on Vercel!"
+        })
     }
