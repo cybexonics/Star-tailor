41a520ac22fd2909ab5e83efafa5c1db6cc8c6c2
cybexonics <cybexonicsitconsultants@gmail.com>
2025-09-29 22:52:03 +0530

    Update upi.py

--- api/settings/upi.py
+++ api/settings/upi.py
@@ -7,40 +7,22 @@ def handler(request):
             "statusCode": 200,
             "headers": {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
             },
             "body": ""
         }
 
-    if request["method"] == "GET":
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT",
-                "Access-Control-Allow-Headers": "Content-Type",
-            },
-            "body": json.dumps({
-                "upi_id": "startailors@upi",
-                "business_name": "STAR TAILORS"
-            })
-        }
-
-    if request["method"] == "PUT":
-        body = json.loads(request.get("body", "{}"))
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT",
-                "Access-Control-Allow-Headers": "Content-Type",
-            },
-            "body": json.dumps({
-                "message": "UPI updated successfully",
-                "upi_id": body.get("upi_id", ""),
-                "business_name": body.get("business_name", "")
-            })
-        }
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+        "body": json.dumps({
+            "upi_id": "startailors@upi",
+            "business_name": "STAR TAILORS"
+        })
+    }
