bc822709f1903bc49088ff49d32ac3b9b29fb8f0
cybexonics <cybexonicsitconsultants@gmail.com>
2025-09-29 23:09:16 +0530

    Create index.py

---
+++ api/tailors/index.py
@@ -0,0 +1,39 @@
+import json
+import uuid
+
+def handler(request):
+    if request["method"] == "OPTIONS":
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+            "body": ""
+        }
+
+    if request["method"] == "GET":
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+            "body": json.dumps({"tailors": []})
+        }
+
+    if request["method"] == "POST":
+        tailor_id = str(uuid.uuid4())
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+            "body": json.dumps({
+                "message": "Tailor created successfully",
+                "_id": tailor_id,
+                "tailor_id": tailor_id
+            })
+        }
+
+    return {
+        "statusCode": 405,
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": json.dumps({"error": "Method not allowed"})
+    }
